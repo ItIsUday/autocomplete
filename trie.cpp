dd3c17a49681ff29a402f2214d935abaeefc6f00
Suraj Mukkannavar <surajrm.is19@rvce.edu.in>
2021-01-17 13:13:28 +0530

    Insert function completed

--- trie.cpp
+++ trie.cpp
@@ -1,7 +1,21 @@
 #include "trie.h"
 
-void Trie::insert(std::string word) {
+void Trie::insert(std::string word)
+{
+    TrieNode *temp = &root;
 
+    for(char level : word)
+    {
+        int index = char_to_index(level);
+
+        if(!temp->children[index])
+        {
+            temp->children[index] = new TrieNode;
+        }
+        temp = temp->children[index];
+    }
+
+    temp->end_of_word = true;
 }
 
 std::vector<std::string> Trie::suggestions(std::string word) {
