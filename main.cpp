61a13ef2f35ec9b0c527b1466a8f65313d6c6b80
Uday <iamuday1618@gmail.com>
2021-01-18 09:44:38 +0530

    Add REPL-like interface
    - Use readline library for shell/REPL-like interface
    - Implement autocompletion
    - Implement command history like functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include "trie.h"
+
+Trie trie;
+
+char *completion_generator(const char *text, int state) {
+    static std::vector<std::string> matches;
+    static size_t match_index = 0;
+
+    if (state == 0) {
+        matches.clear();
+        match_index = 0;
+        matches = trie.suggestions(std::string(text));
+    }
+
+    if (match_index >= matches.size())
+        return nullptr;
+    else
+        return strdup(matches[match_index++].c_str());
+}
+
+char **completer(const char *text, int start, int end) {
+    rl_attempted_completion_over = 1;
+    return rl_completion_matches(text, completion_generator);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "\t\tWelcome to autocompletion program!!" << std::endl;
+    std::cout << "Press <TAB> for autocompletion and press <TAB> twice for displaying all matching words" << std::endl;
+
+    trie = Trie("words.txt");
+
+    rl_attempted_completion_function = completer;
+
+    char *buffer;
+    while ((buffer = readline(">> ")) != nullptr) {
+        if (strlen(buffer) > 0)
+            add_history(buffer);
+        std::cout << '[' << buffer << ']' << std::endl;
+        free(buffer);
+    }
     return 0;
 }
